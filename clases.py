824b76ec02e0ba966959ba30a45420caf7bb123b
Karen Daniela López <Karen_lopez1936@hotmail.com>
2021-03-22 20:02:47 -0700

    Clase 22 de Marzo

--- clases.py
+++ clases.py
@@ -9,31 +9,41 @@ from Bala import *
 tiempo_anterior = 0
 
 carrito = Carrito()
-obstaculo = Obstaculo(0.4, 0.6)
-segundoObstaculo = Obstaculo(-0.5, 0.3)
+
+obstaculos = []
+
+def inicializarObstaculos():
+    global obstaculos
+    obstaculos.append(Obstaculo(0.4, -0.1))
+    obstaculos.append(Obstaculo(-0.5, 0.3))
+    obstaculos.append(Obstaculo(0.5, 0.7))
+    obstaculos.append(Obstaculo(-0.5, -0.7))
+    obstaculos.append(Obstaculo(0.0, -0.4))
 
 def actualizar(window):
     global tiempo_anterior
     global carrito
+    global obstaculos
 
     tiempo_actual = glfw.get_time()
     tiempo_delta = tiempo_actual - tiempo_anterior
 
     carrito.actualizar(window, tiempo_delta)
 
-    carrito.checar_colisiones(obstaculo)
-    if not carrito.colisionando:
-        carrito.checar_colisiones(segundoObstaculo)
+    for obstaculo in obstaculos:
+        if obstaculo.vivo:
+            carrito.checar_colisiones(obstaculo)
+            if carrito.colisionando:
+                break
+
     tiempo_anterior = tiempo_actual
 
 def dibujar():
     global carrito
-    global obstaculo
-    global segundoObstaculo
-
+    global obstaculos
     # rutinas de dibujo
-    obstaculo.dibujar()
-    segundoObstaculo.dibujar()
+    for obstaculo in obstaculos:
+        obstaculo.dibujar()
     carrito.dibujar()
 
 def key_callback(window, key, scancode, action, mods):
@@ -81,6 +91,8 @@ def main():
     print(version_shaders)
 
     glfw.set_key_callback(window, key_callback)
+
+    inicializarObstaculos()
 
     while not glfw.window_should_close(window):
         # Establece regiond e dibujo
